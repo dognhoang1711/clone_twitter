22beb54dbc9b7dbaab84470c5930613de3658c70
donghoang1711 <donghoang17112003@gmail.com>
2024-06-13 09:44:35 +0700

    test push visua

--- routes/auth.php
+++ routes/auth.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'store'])->name('store-register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show-login');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => 'guest'], function () {
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'store'])->name('store-register');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('show-login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
